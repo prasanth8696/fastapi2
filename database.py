37f2d381f4a916c9abee7be9578fd5b89e36b910
prasanth8696 <99348124+prasanth8696@users.noreply.github.com>
2022-08-29 20:51:32 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,10 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./app.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://up9yoqg3nwrxfd3ivvhn:KmLZT8cfN6PxXBjCLGCI@bxhnxyjsoujhavq4refz-postgresql.services.clever-cloud.com:5432/bxhnxyjsoujhavq4refz'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL,
-  connect_args = {'check_same_thread' : False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 
 sessionlocal  = sessionmaker(autocommit=False,autoflush=False,bind=engine)
