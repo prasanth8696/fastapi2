1581fde8bf7819b5950d22cdfc7982161cc569fb
prasanth8696 <99348124+prasanth8696@users.noreply.github.com>
2022-08-30 09:26:39 +0530

    Update home.py

--- home.py
+++ home.py
@@ -3,8 +3,10 @@ from database import sessionlocal
 from models import items
 from typing import List
 from schemas import ShowItemModel
+from auth import get_db
+from sqlalchemy.orm import Session
 from starlette.responses import RedirectResponse
-session = sessionlocal()
+
 
 home = APIRouter(
                  tags = ['HOME'])
@@ -14,14 +16,14 @@ async def get_doc():
  return RedirectResponse('/docs')
 
 @home.get('/items',status_code=200,response_model=List[ShowItemModel])
-async def get_items():
+async def get_items(session:Session=Depends(get_db)):
   items_values = session.query(items).all()
   if items_values :
     return items_values
   else :
    raise HTTPException(status_code=404,detail='Currently dont have items...')
 @home.get('/{item_id}',status_code=200,response_model=ShowItemModel)
-async def get_item_one(item_id : int):
+async def get_item_one(item_id : int,session:Session=Depends(get_db)):
   current_item = session.query(items).filter(items.id == item_id).first()
   if current_item :
     return current_item
